1ddae04d38f5d919e943cb5250b03ee4fe271da6
Ilya Molokovich <im2004v@gmail.com>
2023-01-26 14:02:30 +0600

    Переход на Aiogram

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-Телеграмм бот для студентов НГТУ НЭТИ
+Телеграмм бот на библиотеке aiogram для студентов НГТУ НЭТИ
 Главные возможности:
 TODO: 1. добавление в общий чат группы
 TODO: 2. расписание занятий
@@ -12,31 +12,29 @@ TODO: 7.1. добавление домашнего задания
 TODO: 7.2. уведомление о домашнем задании за установленное время до пары
 '''
 
-import telebot
+from aiogram import Bot, Dispatcher, executor, types
+import logging
 from defs import *
-import requests
-import json
-import datetime
-import time
-import sqlite3
-import os
-import re
-from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
     # Токен бота
     with open('token.txt', 'r') as f:
         token = f.read()
-    bot = telebot.TeleBot(token)
+    bot = Bot(token=token)
+    dp = Dispatcher(bot)
 
-    @bot.message_handler(commands=['start'])
-    def start_command(message):
-        bot.send_message(message.chat.id, 'Привет, я бот для студентов НГТУ НЭТИ')
-        info = f'| {message.from_user.first_name} {message.from_user.last_name}   @{message.from_user.username}\n' \
-               f'| ID: {message.from_user.id}\n| First message: {datetime.datetime.fromtimestamp(message.date).strftime("%d.%m.%Y %H:%M:%S")}'
+    # Логирование
+    logging.basicConfig(level=logging.INFO)
+
+    # Команда /start
+    @dp.message_handler(commands=['start'])
+    async def start_command(message: types.Message):
+        await message.answer('Привет, я бот для студентов НГТУ НЭТИ')
+        info = {'first_name': message.from_user.first_name, 'last_name': message.from_user.last_name,
+                'username': message.from_user.username, 'id': message.from_user.id,
+                'first message': str(message.date)}
         new_one(message.chat.id, info)
 
-
     # запуск бота
-    bot.polling()
+    executor.start_polling(dp)
